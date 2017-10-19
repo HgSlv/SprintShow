de7d3d881bce55a725f17a739ca213d5324eb033
HgSlv <h.silva@epages.com>
2017-10-19 10:44:09 +0200

    Add Mollie links

--- scripts/jiratasks.js
+++ scripts/jiratasks.js
@@ -16,7 +16,11 @@ define(function () {
         "summary": "We are doing a POST request to their payment page, but this needs to be a redirect request.",
         "priority": "Critical",
         "integrated": "7.16.0",
-        "platform": "ePagesJ"
+        "platform": "ePagesJ",
+        "links": [
+            "<a href='https://demounity.barcelona.epages.systems/' target='_blank'>ePages Now SF</a>",
+            "<a href='http://barcelona.epages.systems/epages/DemoShop.mobile' target='_blank'>Mobile SF</a>"
+        ]
     },
     {
         "id": "ESF-1647",

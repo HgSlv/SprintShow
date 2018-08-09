9a577e3202a936e98a6e3cf5e292f6abd01e6509
HgSlv <h.silva@epages.com>
2018-08-09 11:59:33 +0200

    Add links and coverage

--- scripts/jiratasks.js
+++ scripts/jiratasks.js
@@ -21,6 +21,8 @@ define(function () {
         "platform": "ePages6",
         "relevance": "",
         "links": [
+            "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AOrder' target='_blank'>Coverage</a>",
+            "<a href='https://barcelona.epages.systems/epages/DemoShop.admin/sec1cd97a9ebe/?ViewObjectID=21766&ViewAction=MBO-ViewGeneral&ChangeAction=ViewedOn&ChangeObjectID=20928&ListObjectID=21766' target='_blank'>MBO Orders</a>"
 
         ]
     },
